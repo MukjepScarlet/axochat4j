299223da57d6e19bcf254674154a78bc4f544fe5
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-06-17 19:15:17 +0800

    mavenLocal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,29 @@
 allprojects {
-    plugins.apply("java-library")
-
     group = "moe.lasoleil"
     version = "0.1.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
+    }
+}
+
+subprojects {
+    plugins.apply("java-library")
+    plugins.apply("maven-publish")
+
+    afterEvaluate {
+        extensions.findByType<PublishingExtension>()?.apply {
+            publications {
+                create<MavenPublication>("mavenJava") {
+                    from(components.findByName("java"))
+                    groupId = project.group.toString()
+                    artifactId = project.name
+                    version = project.version.toString()
+                }
+            }
+            repositories {
+                mavenLocal()
+            }
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,6 @@ rootProject.name = "axochat4j"
 include(":axochat4j-core")
 include(":axochat4j-codec-gson")
 include(":axochat4j-codec-jackson")
+//include(":axochat4j-client-java11")
 //include(":axochat4j-client-netty")
+//include(":axochat4j-client-okhttp")
